9f38431788081c070f50bcb0344d1b23f1c142ab
Michael Kenji Wilkins <kenji.wilkins@gmail.com>
2024-04-21 14:22:38 +0900

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { LiffProvider } from "./components/LiffProvider";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,7 +17,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <LiffProvider liffId={process.env.NEXT_PUBLIC_LIFF_ID || ""}>
+          {children}
+        </LiffProvider>
+      </body>
     </html>
   );
 }
